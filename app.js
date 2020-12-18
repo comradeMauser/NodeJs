09ed67503a18e4615fa9d3f2d62658ae7b3eebe2
comradeMauser <singularquantumamoeba@gmail.com>
2020-12-18 16:31:24 +0300

    3.14, 3.15: using the node modules system

--- app.js
+++ app.js
@@ -1,46 +1,8 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes')
 
 
-const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method, req.headers)
-    const url = req.url
-    const method = req.method
-
-    if (url === '/') {
-        res.write('<head><title>Enter Message</title></head>')
-        res.write('<body>' +
-            '<form action="/message" method="POST">' +
-            '<input type="text" name="inputMessage"><button type="submit">Send</button></form></body>')
-        return res.end()
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = []
-
-        req.on('data', (chunk) => {
-            // console.log('chunk:', chunk)
-            body.push(chunk)
-        })
-
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString()
-            // console.log('parsedBody:', parsedBody)
-            const messageFromInput = parsedBody.split('=')[1]
-            // console.log('messageFromInput:', messageFromInput)
-            fs.writeFile('message.txt', messageFromInput, () => {
-                res.statusCode = 302
-                res.setHeader('Location', '/')
-                return res.end()
-            })
-        })
-    }
-
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<head><title>Page</title></head>')
-    res.write('<body><h1>Test Server Page</h1></body>')
-    res.end()
-
-    // process.exit()
-})
+const server = http.createServer(routes.handler)
+console.log(routes.text)
 
 server.listen(3000)
