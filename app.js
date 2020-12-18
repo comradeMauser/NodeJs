e83d90bf213cbeb0802c8fc396225e824685b7ea
comradeMauser <singularquantumamoeba@gmail.com>
2020-12-18 14:59:32 +0300

    3.12, 3.13: blocking, non-blocking code

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const server = http.createServer((req, res) => {
     // console.log(req.url, req.method, req.headers)
     const url = req.url
     const method = req.method
+
     if (url === '/') {
         res.write('<head><title>Enter Message</title></head>')
         res.write('<body>' +
@@ -21,18 +22,19 @@ const server = http.createServer((req, res) => {
             body.push(chunk)
         })
 
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString()
             // console.log('parsedBody:', parsedBody)
             const messageFromInput = parsedBody.split('=')[1]
             // console.log('messageFromInput:', messageFromInput)
-            fs.writeFileSync('message.txt', messageFromInput)
+            fs.writeFile('message.txt', messageFromInput, () => {
+                res.statusCode = 302
+                res.setHeader('Location', '/')
+                return res.end()
+            })
         })
-
-        res.statusCode = 302
-        res.setHeader('Location', '/')
-        return res.end()
     }
+
     res.setHeader('Content-Type', 'text/html')
     res.write('<head><title>Page</title></head>')
     res.write('<body><h1>Test Server Page</h1></body>')
