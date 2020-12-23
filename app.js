be3ad6100cc4311126da49b14a02b1b52d3c56d0
comradeMauser <singularquantumamoeba@gmail.com>
2020-12-23 16:40:31 +0300

    5.(12-16): filtering paths, serving HTML pages

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const adminRoutes = require('./routes/admin.js')
 const shopRoutes = require('./routes/shop.js')
+const path = require('path')
+
 
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(adminRoutes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
+
+//404 case
+app.use((req, res, next) => {
+    console.log("4O4")
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
+})
 
 app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,14 @@
 const express = require('express')
-
 const router = express.Router()
+const path = require('path')
 
+// /admin/add-product ==> GET
 router.get('/add-product', (req, res, next) => {
-    res.send("<h1>Add product page</h1>" +
-        "<form action='/products' method='POST'>" +
-        "<input type='text' name='title'> " +
-        "<button type='submit'>add product</button>" +
-        "</form>")
+    res.sendFile(path.join(__dirname, "../", "views", "add-product.html"))
 })
 
-router.post('/products', (req, res, next) => {
+// /admin/add-product ==> POST
+router.post('/add-product', (req, res, next) => {
     console.log(req.body)
     res.redirect('/')
 })
