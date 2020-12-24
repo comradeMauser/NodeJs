9982a5f06631414ea91b3a3fd0a0863128c2f0f6
comradeMauser <singularquantumamoeba@gmail.com>
2020-12-24 23:17:08 +0300

    5.16, 5.17: 404 page, using a helper function for navigation

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const rootDir = require('../utils/path.js')
+
 
 // /admin/add-product ==> GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, "../", "views", "add-product.html"))
+    res.sendFile(path.join(rootDir, "views", "add-product.html"))
 })
 
 // /admin/add-product ==> POST
