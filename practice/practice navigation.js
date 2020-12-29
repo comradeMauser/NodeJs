ddd462362a59f0a231a59a68d309e5bd0bb38f8f
comradeMauser <singularquantumamoeba@gmail.com>
2020-12-30 00:12:28 +0300

    6.1, 6.2: sharing data across requests/users

---
+++ practice/practice navigation.js
@@ -0,0 +1,11 @@
+// part 5(2) practice navigation
+
+const express = require('express')
+const path = require('path')
+const mainRoutes = require('./routes/index')
+
+const app = express()
+app.use(express.static(path.join(__dirname, "public")))
+app.use(mainRoutes)
+
+app.listen(3001)
