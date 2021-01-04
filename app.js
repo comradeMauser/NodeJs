94d1284f2e75992eecec273289c111a8492d51df
comradeMauser <singularquantumamoeba@gmail.com>
2021-01-04 16:52:38 +0300

    6.14: working with EJS

--- app.js
+++ app.js
@@ -3,17 +3,12 @@ const bodyParser = require('body-parser')
 const adminData = require('./routes/admin.js')
 const shopRoutes = require('./routes/shop.js')
 const path = require('path')
-const expressHbs = require('express-handlebars')
+// const expressHbs = require('express-handlebars')
 
 
 const app = express()
 
-app.engine("hbs", expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-}))
-app.set("view engine", "hbs")
+app.set("view engine", "ejs")
 // app.set("view engine", "pug")
 app.set("views", "views")
 
